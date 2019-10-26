cb67e8dcd0f574ee35eb6b67e3694ce3daebfb4d
bear2u <bear2u@naver.com>
2019-10-26 15:18:12 +0900

    Remote에 로직 추가

--- app/src/main/java/com/example/bookstore/logic/remote/RemoteDataSourceImpl.java
+++ app/src/main/java/com/example/bookstore/logic/remote/RemoteDataSourceImpl.java
@@ -1,15 +1,21 @@
 package com.example.bookstore.logic.remote;
 
+import android.util.Log;
+
 import com.example.bookstore.logic.DataSource;
 import com.example.bookstore.logic.Repository;
+import com.example.bookstore.logic.remote.services.MovieApiService;
 import com.example.bookstore.model.Movie;
+import com.example.bookstore.model.ResponseMovie;
 
 import java.util.List;
 
 import io.reactivex.Single;
+import io.reactivex.functions.Function;
 
 public class RemoteDataSourceImpl implements DataSource {
     Repository repository;
+    private String _TAG = "Remote";
 
     @Override
     public void setRepository(Repository repository) {
@@ -21,6 +27,14 @@ public class RemoteDataSourceImpl implements DataSource {
         // Repository 를 리턴
         //TODO 서버연동 및 데이터 변환
         //TODO 레트로핏 연동 / GSON / RxJava
-        return null;
+        MovieApiService service = NetRetrofit.getInstance().getRetrofit().create(MovieApiService.class);
+        return service.fetchMovies()
+                .map(new Function<ResponseMovie, List<Movie>>() {
+                    @Override
+                    public List<Movie> apply(ResponseMovie responseMovie) throws Exception {
+                        Log.d(_TAG, "responseMovie " + responseMovie.toString());
+                        return responseMovie.getData().getMovies();
+                    }
+                });
     }
 }
